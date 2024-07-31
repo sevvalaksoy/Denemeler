12f54f5a3f37947916381e20f790433c0d790a2e
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-07-31 15:58:48 +0300

    day2q1

---
+++ src/com/workintech/example/FirstUniqCharacter.java
@@ -0,0 +1,38 @@
+package com.workintech.example;
+/* https://leetcode.com/problems/first-unique-character-in-a-string/submissions/1339536567 */
+/* Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1. */
+public class FirstUniqCharacter {
+    public static void main(String[] args) {
+        firstUniqChar("loveleetcode");
+    }
+    public static int firstUniqChar(String s) {
+        char[] characters = s.toCharArray();
+        if(characters.length==1){
+            System.out.println(0);
+            return 0;
+        }
+        int[] indexes = new int[characters.length];
+        for (int i = 0; i < characters.length; i++ ){
+            if(indexes[i]!=0){
+                continue;
+            }
+            if(i==characters.length-1){
+                System.out.println(i);
+                return i;
+            }
+            boolean isSingle = true;
+            for ( int j = i+1; j < characters.length; j++){
+                if(characters[i]==characters[j]){
+                    indexes[j] = j;
+                    isSingle = false;
+                }
+            }
+            if(isSingle){
+                System.out.println(i);
+                return i;
+            }
+        }
+        System.out.println(-1);
+        return -1;
+    }
+}
