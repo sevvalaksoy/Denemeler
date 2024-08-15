d0fefbc9f107f9508ed6e650a5077efa8c84e335
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-08-15 10:33:08 +0300

    22nd

--- src/com/workintech/example/ConstructTheRectangle.java
+++ src/com/workintech/example/ConstructTheRectangle.java
@@ -1,6 +1,16 @@
 package com.workintech.example;
 
-import java.util.Iterator;
+/* https://leetcode.com/problems/construct-the-rectangle/submissions/1356276666 */
+
+/*A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+
+The area of the rectangular web page you designed must equal to the given target area.
+The width W should not be larger than the length L, which means L >= W.
+The difference between length L and width W should be as small as possible.
+Return an array [L, W] where L and W are the length and width of the web page you designed in sequence.
+
+*/
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -20,5 +30,11 @@ public class ConstructTheRectangle {
             System.out.println(x);
         }
         return r.get(r.size()/2);
+    }
+    public static int[] constructRectangleII(int area) {
+        for(int i=(int)(Math.floor(Math.sqrt(area)));i>0;i--){
+            if(area%i==0)return new int[]{area/i,i};
+        }
+        return new int[]{area,1};
     }
 }
