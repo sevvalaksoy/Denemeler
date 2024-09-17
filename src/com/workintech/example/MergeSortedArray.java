2526301b2e1ae6dc1c25a7f15e34b3c67b5a78b1
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-09-17 11:23:14 +0300

    35th

---
+++ src/com/workintech/example/MergeSortedArray.java
@@ -0,0 +1,29 @@
+package com.workintech.example;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+        merge(nums1,m, nums2, n);
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while( j >= 0 && i >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(j >= 0){
+            nums1[k--] = nums2[j--];
+        }
+        for(int x:nums1){
+            System.out.println(x);
+        }
+    }
+}
